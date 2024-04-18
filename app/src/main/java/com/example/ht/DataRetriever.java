59ab78017e40805667a20d34f2239c137ad6efe6
aaboeh <aapo.aarvala@gmail.com>
2024-04-18 06:34:13 -0700

    Second Commit

--- app/src/main/java/com/example/ht/DataRetriever.java
+++ app/src/main/java/com/example/ht/DataRetriever.java
@@ -1,8 +1,119 @@
 package com.example.ht;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class DataRetriever {
 
     public void getPopulationData(String municipality) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        JsonNode areas = null;
+
+        try {
+            areas = objectMapper.readTree(new URL("https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px"));
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        ArrayList<String> keys = new ArrayList<>();
+        ArrayList<String> values = new ArrayList<>();
+
+        for (JsonNode node : areas.get("variables").get(1).get("values")) {
+            values.add(node.asText());
+        }
+        for (JsonNode node : areas.get("variables").get(1).get("valueTexts")) {
+            keys.add(node.asText());
+        }
+
+        HashMap<String, String> municipalityCodes = new HashMap<>();
+
+        for(int i = 0; i < keys.size(); i++) {
+            municipalityCodes.put(keys.get(i), values.get(i));
+        }
+
+        String code = null;
+
+
+        code = null;
+        code = municipalityCodes.get(municipality);
+
+        if (code == null) {
+            break;
+        }
+
+        try {
+            URL url = new URL("https://pxdata.stat.fi:443/PxWeb/api/v1/fi/StatFin/synt/statfin_synt_pxt_12dy.px");
+
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json; utf-8");
+            con.setRequestProperty("Accept", "application/json");
+            con.setDoOutput(true);
+
+            JsonNode jsonInputString = objectMapper.readTree(new File("query.json"));
+
+            ((ObjectNode) jsonInputString.get("query").get(0).get("selection")).putArray("values").add(code);
+
+            byte[] input = objectMapper.writeValueAsBytes(jsonInputString);
+            OutputStream os = con.getOutputStream();
+            os.write(input, 0, input.length);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
+            StringBuilder response = new StringBuilder();
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                response.append(line.trim());
+            }
+
+            JsonNode municipalityData = objectMapper.readTree(response.toString());
+
+            ArrayList<String> years = new ArrayList<>();
+            ArrayList<String> populations = new ArrayList<>();
+
+            for (JsonNode node : municipalityData.get("dimension").get("Vuosi").get("category").get("label")) {
+                years.add(node.asText());
+            }
+
+            for (JsonNode node : municipalityData.get("value")) {
+                populations.add(node.asText());
+            }
+
+            ArrayList<MunicipalityData> populationData = new ArrayList<>();
+
+            for(int i = 0; i < years.size(); i++) {
+                populationData.add(new MunicipalityData(Integer.valueOf(years.get(i)), Integer.valueOf(populations.get(i))));
+            }
+
+            for (MunicipalityData data : populationData) {
+                System.out.print(data.getYear() + ": " + data.getPopulation() + "  ");
+                for(int i = 0; i < data.getPopulation() / 10000; i++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
 
     }
 }
